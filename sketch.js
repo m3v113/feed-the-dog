b15a797b859afdb94822ab62793bbea08f749dc7
m3v113 <76668066+m3v113@users.noreply.github.com>
2021-05-14 09:39:59 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+var lastFed, feed;
+//create feed and lastFed variable here
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+
+  feed = createButton ("Feed the DOg");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+  var readFeedtimeRef = database.ref('Feedtime');
+  readFeedtimeRef.on("value", function(data){
+    lastFed = data.val();
+  })
+ 
+  //write code to display text lastFed time here
+  console.log(hour());
+  
+  displayTime();
+
+  
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  //write code here to update food stock and last fed time
+  var food_stock_val = foodObj.getFoodStock();
+  if (food_stock_val <= 0) {
+    foodObj.updateFoodStock(food_stock_val *0);
+  }
+  else {
+    foodObj.updateFoodStock(food_stock_val -1)
+  }
+
+  database.ref('/').update({
+    'Feedtime': hour()
+  })
+
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+function displayTime() {
+  fill("black");
+  if (lastFed >= 12) {
+    text("Last Feed: " + hour() + " PM", 350, 30);
+  }
+
+  else if (lastFed == 0) {
+    text("Last Feed: 12 AM", 350, 30)
+  }
+
+  else {
+    text("Last Feed: " + hour() + " AM", 350, 30);
+  }
+}
